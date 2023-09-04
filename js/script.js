e4a788616f6cc67627db1858d00dd97fc193635d
SebastianMonsalve <sebastiandev16@gmail.com>
2023-09-04 14:15:08 -0500

    Blog de consultas

---
+++ js/script.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+    reset: true,
+    distance: '5rem',
+    duration: 1900,
+    delay:80
+    });
+
+ScrollReveal().reveal('.ult-card, .card, .btn, .port', { origin: 'bottom' });
+ScrollReveal().reveal('.ult-content h4, .title', { origin: 'left' });
+ScrollReveal().reveal('.ult-card img', { origin: 'right' });
+ScrollReveal().reveal('.title', { origin: 'top' });
+ScrollReveal().reveal('.header-content h1, .header-content h2, .header-content p', { origin: 'center' });
