cbe0a043e7676f997b21b5d390e462309e08883a
SebastianMonsalve <sebastiandev16@gmail.com>
2023-09-05 13:53:24 -0500

    Protocolos

---
+++ js/scriptArticulo.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset: true,
+    distance: '5rem',
+    duration: 1200,
+    delay:40
+    });
+
+ScrollReveal().reveal('.right-index, .right-index img', { origin: 'right' });
+ScrollReveal().reveal('.title, .line', { origin: 'top' });
